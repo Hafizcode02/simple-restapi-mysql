031a76fdfb1a0ac8bc1b5b1cd52a2c6ca3bd02f3
Hafiz Caniago <hafizcode02@gmail.com>
2023-11-19 14:30:51 +0700

    indentation

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const UserController = require('../controllers/users.js');
-
 const router = express.Router();
 
 router.get('/', UserController.getAllUsers);
-
 router.post('/', UserController.createNewUser);
 
 module.exports = router
